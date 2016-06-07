699002a650ee2a0fe2714830969ae36da3ded528
Narendra Shetty <mailnarendra15@gmail.com>
2016-06-07 09:21:39 +0530

    have setup the build process

--- build/index.js
+++ build/index.js
@@ -1,1 +1,44 @@
-console.log('test');
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = ReactNativeShorthand;
+function parseStyles(styles) {
+  var supportedShorthand = ['margin', 'padding'];
+  var directionX = ['left', 'right'];
+  var directionY = ['top', 'bottom'];
+  var result = {};
+
+  for (var rule in this.styles) {
+    result[rule] = {};
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+      for (var _iterator = rule[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var selector = _step.value;
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+          _iterator.return();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
+  }
+}
+
+function ReactNativeShorthand() {
+  var styles = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+
+  return parseStyles(styles);
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+function parseStyles(styles) {
+  const supportedShorthand = ['margin', 'padding'];
+  const directionX = ['left', 'right'];
+  const directionY = ['top', 'bottom'];
+  const result = {};
+
+  
+}
+
+export default function ReactNativeShorthand(styles = {}) {
+  return parseStyles(styles);
+}
